2605efc74a2f264cdaf7930c23b5fa0a843381d2
AlarconCode <julian.alarcon81@gmail.com>
2023-01-02 20:26:54 +0100

    getting activities añadiendo html y css basico

--- apiRest/strava/strava-api.js
+++ apiRest/strava/strava-api.js
@@ -1,24 +1,61 @@
 const auth_link = 'https://www.strava.com/oauth/token'
 
-const getActivities = async (access_token) => {
+const getActivities = async (token) => {
+
+  
     
     try {
-        const url = `https://www.strava.com/api/v3/athlete/activities?access_token=${access_token}`
-        let response = await fetch(url)
-        let data = await response.json()
-        console.log(data);
+        let before = Date.now()
+        console.log(before);
+        const urlList1 = `https://www.strava.com/api/v3/athlete/activities?before=${Date.parse('2023/1/1')}&page=10&per_page=30&access_token=${token.access_token}`
+        let resList = await fetch(urlList1)
+        let listActivities = await resList.json()
+        console.log(listActivities);
 
-        const container = document.getElementById('app')
-    
-        data.forEach(element => {
-        container.innerHTML += `<h2>${element.name}<span>  Distancia: ${element.distance}</span></h2>`
-    });
-     
 
-       } catch (error) {
-            console.log(error);
-       }
+        const urlPage = `https://www.strava.com/api/v3/activities/${listActivities[29].id}?include_all_efforts=true&access_token=${token.access_token}`
+        let resAct = await fetch(urlPage)
+        let activity = resAct.json()
+        console.log(activity);
+
+
+        const container = document.getElementById('container')
     
+        listActivities.forEach(activity => {
+        container.innerHTML += `
+        <div class="card-activity">
+            <h2 class="title">${activity.name}</h2>
+            <p>Distancia: ${activity.distance/1000}kms</p>
+            <p>Fecha:${activity.start_date}</p>
+        </div>    
+        `
+       });
+    }
+    
+    catch (error) {
+        console.log(error);
+    }
+    
+    // try {
+    //     let before = new Date(2022,9,10)
+    //     console.log(before);
+    //     const urlList1 = `https://www.strava.com/api/v3/athlete/activities?before=${before.getTime()}&access_token=${token.access_token}`
+    //     let resList = await fetch(urlList1)
+    //     let listActivities = await resList.json()
+    //     console.log(listActivities);
+
+    //     const container = document.getElementById('app')
+    
+    //     listActivities.forEach(activity => {
+    //     container.innerHTML += `
+    //     <h1>Segundo</h1>
+    //     <h2>${activity.name}<span>  Distancia: ${activity.distance}</span></h2>
+    //     <a href="">ver más</a>
+    //     `
+    //     });
+    // }  catch (error) {
+    //     console.log(error);
+    // } 
 
     
 
@@ -45,10 +82,9 @@ const reAuthorize = async () => {
     
     try {
         let response = await fetch(auth_link, options)
-        let data = await response.json()
-        console.log(data);
-        let access_token = data.access_token
-        getActivities(access_token)
+        let token = await response.json()
+        console.log(token);
+        getActivities(token)
         
     } catch (error) {
         console.log(error);
